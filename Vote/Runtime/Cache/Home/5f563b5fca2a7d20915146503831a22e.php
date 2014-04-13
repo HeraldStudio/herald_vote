6297fdcbe7bbf45e34b23acee4aeee6d8e81f7c9
Tairy <tairyguo@gmail.com>
2014-04-13 21:03:13 +0800

    完成首页

--- Vote/Runtime/Cache/Home/5f563b5fca2a7d20915146503831a22e.php
+++ Vote/Runtime/Cache/Home/5f563b5fca2a7d20915146503831a22e.php
@@ -6,30 +6,20 @@
 	<title>先声投票</title>
 	<link rel="stylesheet" type="text/css" href="/herald_vote/Public/Css/bootstrap.css" />
 	<link rel="stylesheet" type="text/css" href="/herald_vote/Public/Css/style-home.css" />
+	<link rel="stylesheet" type="text/css" href="/herald_vote/Public/Css/app.css" />
 </head>
 <body>
-	<header class="navbar navbar-default bs-docs-nav" role="banner">
-		<div class="container">
-			<div class="navbar-header">
-				<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				</button>
-				<a href="../" class="navbar-brand">先声投票</a>
-			</div>
-			<nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
-				<ul class="nav navbar-nav navbar-right">
-					<li>
-						<a href="http://herald.seu.edu.cn">先声首页</a>
-					</li>
-					<li>
-						<a href="http://herald.seu.edu.cn/about/">关于我们</a>
-					</li>
-				</ul>
-			</nav>
-		</div>
+	<header>
+    <div class="container clearfix">
+        <h1 id="logo">
+            投票啦
+        </h1>
+        <nav>
+            <a href="">首页</a>
+            <a href="">关于</a>
+            <a href="">投票</a>
+        </nav>
+    </div>
 	</header>
 <div class="container">
 	<div class="row">
@@ -155,6 +145,24 @@
 <script type="text/javascript" src="/herald_vote/Public/Js/jquery.js"></script>
 <script type="text/javascript" src="/herald_vote/Public/Js/bootstrap.js"></script>
 <script type="text/javascript" src="/herald_vote/Public/Js/video.js"></script>
+<script type="text/javascript" src="/herald_vote/Public/Js/rollheader.js"></script>
 <script type="text/javascript" src="/herald_vote/Public/Js/herald-vote-home.js"></script>
+<script>
+  function init() {
+      window.addEventListener('scroll', function(e){
+          var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+              shrinkOn = 300,
+              header = document.querySelector("header");
+          if (distanceY > shrinkOn) {
+              classie.add(header,"smaller");
+          } else {
+              if (classie.has(header,"smaller")) {
+                  classie.remove(header,"smaller");
+              }
+          }
+      });
+  }
+  window.onload = init();
+</script>
 </body>
 </html>
--- Vote/Runtime/Cache/Home/fe76a870fab4f59f28dbabb55bc44b54.php
+++ Vote/Runtime/Cache/Home/fe76a870fab4f59f28dbabb55bc44b54.php
@@ -6,32 +6,180 @@
 	<title>先声投票</title>
 	<link rel="stylesheet" type="text/css" href="/herald_vote/Public/Css/bootstrap.css" />
 	<link rel="stylesheet" type="text/css" href="/herald_vote/Public/Css/style-home.css" />
+	<link rel="stylesheet" type="text/css" href="/herald_vote/Public/Css/app.css" />
 </head>
 <body>
-	<header class="navbar navbar-default bs-docs-nav" role="banner">
-		<div class="container">
-			<div class="navbar-header">
-				<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				</button>
-				<a href="../" class="navbar-brand">先声投票</a>
-			</div>
-			<nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
-				<ul class="nav navbar-nav navbar-right">
-					<li>
-						<a href="http://herald.seu.edu.cn">先声首页</a>
-					</li>
-					<li>
-						<a href="http://herald.seu.edu.cn/about/">关于我们</a>
-					</li>
-				</ul>
-			</nav>
-		</div>
+	<header>
+    <div class="container clearfix">
+        <h1 id="logo">
+            投票啦
+        </h1>
+        <nav>
+            <a href="">首页</a>
+            <a href="">关于</a>
+            <a href="">投票</a>
+        </nav>
+    </div>
 	</header>
-ssssasasa
+<div class="">
+	<div id="carousel-example-captions" class="carousel slide" data-ride="carousel">
+		<ol class="carousel-indicators">
+			<li data-target="#carousel-example-captions" data-slide-to="0" class=""></li>
+			<li data-target="#carousel-example-captions" data-slide-to="1" class="active"></li>
+			<li data-target="#carousel-example-captions" data-slide-to="2" class=""></li>
+		</ol>
+		<div class="carousel-inner">
+			<div class="item">
+				<img class="item-img" alt="bg-1" src="/herald_vote/Public/Images/bg-1.jpg">
+					<div class="carousel-caption">
+					<h3>First slide label</h3>
+					<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+				</div>
+			</div>
+			<div class="item active">
+				<img class="item-img" src="/herald_vote/Public/Images/bg-2.jpg">
+					<div class="carousel-caption">
+					<h3>Second slide label</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				</div>
+			</div>
+			<div class="item">
+				<img class="item-img" src="/herald_vote/Public/Images/bg-3.jpg">
+					<div class="carousel-caption">
+					<h3>Third slide label</h3>
+					<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+				</div>
+			</div>
+		</div>
+		<a class="left carousel-control" href="#carousel-example-captions" data-slide="prev">
+		<span class="glyphicon glyphicon-chevron-left"></span>
+		</a>
+		<a class="right carousel-control" href="#carousel-example-captions" data-slide="next">
+		<span class="glyphicon glyphicon-chevron-right"></span>
+		</a>
+	</div>
+</div>
+
+<section class="last-vote">
+  <div class="container">
+    <div class="presentazione">
+    	<center>
+    		<h2>最新投票</h2>
+      	<p>dd</p>
+    	</center>
+    </div>
+    <div class="row">
+      <div class="col-sm-4">
+        <div class="media">
+          <div class="row ">
+            <div class="media-img  pull-left  col-sm-5  col-md-4 ">
+              <div class="img-overlay "> 
+              	<a class="btn  btn-primary  animated fadeInUp " href="/about/" rel="bookmark">快去投票</a> 
+              </div>
+              <img src="/herald_vote/Public/Images/bg-6.jpg" alt="About" width="150" height="150"> 
+            </div>
+            <div class="media-body   col-sm-7 col-md-8  ">
+              <h4 class="media-heading"> 
+              	测试投票
+              </h4>
+              <p class="hidden-sm">测试投票测试投票测试投票测试投票测试投票测试投票测试投票测试投票</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="media">
+            <div class="row ">
+            <div class="media-img  pull-left  col-sm-5  col-md-4 ">
+              <div class="img-overlay "> 
+              	<a class="btn  btn-primary  animated fadeInUp " href="/about/" rel="bookmark">快去投票</a> 
+              </div>
+              <img src="/herald_vote/Public/Images/bg-6.jpg" alt="About" width="150" height="150"> 
+            </div>
+            <div class="media-body   col-sm-7 col-md-8  ">
+              <h4 class="media-heading"> 
+              	测试投票
+              </h4>
+              <p class="hidden-sm">测试投票测试投票测试投票测试投票测试投票测试投票测试投票测试投票</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="media">
+            <div class="row ">
+            <div class="media-img  pull-left  col-sm-5  col-md-4 ">
+              <div class="img-overlay "> 
+              	<a class="btn  btn-primary  animated fadeInUp " href="/about/" rel="bookmark">快去投票</a> 
+              </div>
+              <img src="/herald_vote/Public/Images/bg-6.jpg" alt="About" width="150" height="150"> 
+            </div>
+            <div class="media-body   col-sm-7 col-md-8  ">
+              <h4 class="media-heading"> 
+              	测试投票
+              </h4>
+              <p class="hidden-sm">测试投票测试投票测试投票测试投票测试投票测试投票测试投票测试投票</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="media">
+            <div class="row ">
+            <div class="media-img  pull-left  col-sm-5  col-md-4 ">
+              <div class="img-overlay "> 
+              	<a class="btn  btn-primary  animated fadeInUp " href="/about/" rel="bookmark">快去投票</a> 
+              </div>
+              <img src="/herald_vote/Public/Images/bg-6.jpg" alt="About" width="150" height="150"> 
+            </div>
+            <div class="media-body   col-sm-7 col-md-8  ">
+              <h4 class="media-heading"> 
+              	测试投票
+              </h4>
+              <p class="hidden-sm">测试投票测试投票测试投票测试投票测试投票测试投票测试投票测试投票</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="media">
+            <div class="row ">
+            <div class="media-img  pull-left  col-sm-5  col-md-4 ">
+              <div class="img-overlay "> 
+              	<a class="btn  btn-primary  animated fadeInUp " href="/about/" rel="bookmark">快去投票</a> 
+              </div>
+              <img src="/herald_vote/Public/Images/bg-6.jpg" alt="About" width="150" height="150"> 
+            </div>
+            <div class="media-body   col-sm-7 col-md-8  ">
+              <h4 class="media-heading"> 
+              	测试投票
+              </h4>
+              <p class="hidden-sm">测试投票测试投票测试投票测试投票测试投票测试投票测试投票测试投票</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="media">
+            <div class="row ">
+            <div class="media-img  pull-left  col-sm-5  col-md-4 ">
+              <div class="img-overlay "> 
+              	<a class="btn  btn-primary  animated fadeInUp " href="/about/" rel="bookmark">快去投票</a> 
+              </div>
+              <img src="/herald_vote/Public/Images/bg-6.jpg" alt="About" width="150" height="150"> 
+            </div>
+            <div class="media-body   col-sm-7 col-md-8  ">
+              <h4 class="media-heading"> 
+              	测试投票
+              </h4>
+              <p class="hidden-sm">测试投票测试投票测试投票测试投票测试投票测试投票测试投票测试投票</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 <footer>
 	<hr>
 	<center>
@@ -43,6 +191,25 @@ ssssasasa
 
 <script type="text/javascript" src="/herald_vote/Public/Js/jquery.js"></script>
 <script type="text/javascript" src="/herald_vote/Public/Js/bootstrap.js"></script>
+<script type="text/javascript" src="/herald_vote/Public/Js/video.js"></script>
+<script type="text/javascript" src="/herald_vote/Public/Js/rollheader.js"></script>
 <script type="text/javascript" src="/herald_vote/Public/Js/herald-vote-home.js"></script>
+<script>
+  function init() {
+      window.addEventListener('scroll', function(e){
+          var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+              shrinkOn = 300,
+              header = document.querySelector("header");
+          if (distanceY > shrinkOn) {
+              classie.add(header,"smaller");
+          } else {
+              if (classie.has(header,"smaller")) {
+                  classie.remove(header,"smaller");
+              }
+          }
+      });
+  }
+  window.onload = init();
+</script>
 </body>
 </html>
